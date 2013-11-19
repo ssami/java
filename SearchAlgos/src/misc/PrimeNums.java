26d786963fbe94193ec51a3b444793304c0c6271
ssami <ssami@ebay.com>
2013-11-19 09:35:13 -0800

    Prime Numbers, the sieve

--- SearchAlgos/src/misc/PrimeNums.java
+++ SearchAlgos/src/misc/PrimeNums.java
@@ -18,20 +18,35 @@ public class PrimeNums {
 		int p = 2 ;
 		
 		while (p < maxPrime){
-			for (int i=0; i<maxPrime; i++){
-				if (i % p == 0){
+			for (int i=allNums.indexOf(p)+1; i<maxPrime; i++){
+				if (allNums.get(i) % p == 0){
 					allNums.set(i, -1); 
 				}
 			}
 			
-			int c = allNums.indexOf(p)+1;  
-			while (allNums.get(c) == -1){
+			int c = allNums.indexOf(p)+1;
+			while (c < maxPrime && allNums.get(c) == -1){
 				c++; 
+				
 			}
-			p = allNums.get(c); 
+			if (c < maxPrime){
+				p = allNums.get(c);
+			}
+			else {
+				p = maxPrime; 
+			}
 		}
 		
 		
+		for (int i=0; i<maxPrime; i++){
+			if (allNums.get(i) != -1){
+				System.out.println("Prime: " + allNums.get(i));
+			}
+		}
 		
+	}
+	
+	public static void main (String args[]){
+		PrimeNums.findPrimes(20);
 	}
 }
